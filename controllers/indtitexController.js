fc8b9f70eba094694aceb6a4d6683d7b377fb124
MilosMrdja <mrdja.sv34.2022@uns.ac.rs>
2025-05-03 15:06:58 +0200

    feat: add inditex api

--- controllers/indtitexController.js
+++ controllers/indtitexController.js
@@ -1,8 +1,22 @@
-exports.test = (req, res, next) => {
-  console.log("hello");
-  res.status(200).json({
-    status: "success",
-    // body: {    }
-  });
+const performVisualSearch = require("../services/visualSearchService")
+
+
+exports.visualSearch = async (req, res, next) => {
+  try {
+    const imageUrl = req.query.image;
+    const result = await performVisualSearch.performVisualSearch(imageUrl);
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: 'There are no products' });
+    }
+
+    res.status(200).json({
+      status: "success",
+      result
+    })
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).json({ message: 'Greška u vizuelnoj pretrazi' });
+  }
   next();
 };
